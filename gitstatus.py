891077c93244c14cf906abb3d643582a5dbc5217
thuan0311 <bachthuan03111997@gmail.com>
2020-03-17 09:50:06 +0700

    adding gitstatus file

---
+++ gitstatus.py
@@ -0,0 +1,17 @@
+git status 
+git checkout 
+git branch 
+git add
+git push 
+git commit
+
+
+echo "# test-git" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/thuan0311/test-git.git
+git push -u origin master
+
+git remote add origin https://github.com/thuan0311/test-git.git
+git push -u origin master
